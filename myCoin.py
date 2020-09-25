d7b0757dab4e15ed65932d544a3c824885ba2147
Adrijan <45051889+adrijano@users.noreply.github.com>
2020-09-25 14:44:05 +0200

    Add files via upload

---
+++ myCoin.py
@@ -0,0 +1,199 @@
+# myCoin brainwallet
+# Created by A. Petek
+
+import hashlib
+import os
+import random
+import binascii
+import ecdsa
+import base58
+import PySimpleGUI as sg
+import pyperclip
+import webbrowser
+import requests
+from json import (load as jsonload, dump as jsondump)
+from os import path
+import json
+import blockcypher
+
+
+def sal():
+    return binascii.hexlify(os.urandom(8)).decode('utf-8')
+
+def seed(f, salt):
+    p = f+ ' ' +salt
+    return hashlib.sha256(p.encode("utf-8")).hexdigest()
+
+
+
+def pub_key(secret_exponent):
+    key = binascii.unhexlify(secret_exponent)
+    s = ecdsa.SigningKey.from_string(key, curve = ecdsa.SECP256k1)
+    return '04' + binascii.hexlify(s.verifying_key.to_string()).decode('utf-8')
+
+def addr(public_key):
+    output = []; alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+    var = hashlib.new('ripemd160')
+    var.update(hashlib.sha256(binascii.unhexlify(public_key.encode())).digest())
+    var = '00' + var.hexdigest() + hashlib.sha256(hashlib.sha256(binascii.unhexlify(('00' + var.hexdigest()).encode())).digest()).hexdigest()[0:8]
+    count = [char != '0' for char in var].index(True) // 2
+    n = int(var, 16)
+    while n > 0:
+        n, remainder = divmod(n, 58)
+        output.append(alphabet[remainder])
+    for i in range(count): output.append(alphabet[0])
+    return ''.join(output[::-1])
+
+def wif(secret_exponent):
+    var80 = "80"+secret_exponent
+    var = hashlib.sha256(binascii.unhexlify(hashlib.sha256(binascii.unhexlify(var80)).hexdigest())).hexdigest()
+    return str(base58.b58encode(binascii.unhexlify(str(var80) + str(var[0:8]))), 'utf-8')
+
+
+def bala(address):
+    return blockcypher.get_total_balance(address)
+
+def over(address):
+    return blockcypher.get_num_confirmed_transactions(address)
+
+
+
+SETTINGS_FILE = path.join(path.dirname(__file__), r'settings_file.cfg')
+DEFAULT_SETTINGS = {'theme': sg.theme()}
+SETTINGS_KEYS_TO_ELEMENT_KEYS = {'theme': '-THEME-'}
+
+def load_settings(settings_file, default_settings):
+    try:
+        with open(settings_file, 'r') as f:
+            settings = jsonload(f)
+    except Exception as e:
+        sg.popup_quick_message(f'exception {e}', 'No settings file found... will create one for you', keep_on_top=True, background_color='red', text_color='white')
+        settings = default_settings
+        save_settings(settings_file, settings, None)
+    return settings
+
+
+def save_settings(settings_file, settings, values):
+    if values:      
+        for key in SETTINGS_KEYS_TO_ELEMENT_KEYS:  
+            try:
+                settings[key] = values[SETTINGS_KEYS_TO_ELEMENT_KEYS[key]]
+            except Exception as e:
+                print(f'Problem updating settings from window values. Key = {key}')
+
+    with open(settings_file, 'w') as f:
+        jsondump(settings, f)
+
+    sg.popup('Settings saved')
+
+def create_settings_window(settings):
+    sg.theme(settings['theme'])
+
+    def TextLabel(text): return sg.Text(text+':', justification='r', size=(15,1))
+
+    layout = [  [sg.Text('Settings', font='Any 15')],
+                [TextLabel('Theme'),sg.Combo(sg.theme_list(), size=(20, 20), key='-THEME-')],
+                [sg.Button('Save'), sg.Button('Exit')]  ]
+
+    window = sg.Window('Settings', layout, keep_on_top=True, finalize=True)
+
+    for key in SETTINGS_KEYS_TO_ELEMENT_KEYS:
+        try:
+            window[SETTINGS_KEYS_TO_ELEMENT_KEYS[key]].update(value=settings[key])
+        except Exception as e:
+            print(f'Problem updating PySimpleGUI window from settings. Key = {key}')
+
+    return window
+
+
+
+
+        
+def create_main_window(settings):
+    sg.theme(settings['theme'])
+    menu_def = [['&Menu', ['&Copy', '&Paste','&Settings', 'E&xit']],
+                ['&Help', '&About...']]
+
+    right_click_menu = ['Unused', ['&Copy', '&Paste','Settings', 'E&xit']]
+
+    layout =  [[sg.Menu(menu_def)],
+               [sg.Image('bit.png'), sg.Text('', size=(27,1)), sg.Button('', key='paypal', size=(12,1), button_color=(sg.theme_background_color(), sg.theme_background_color()),
+                                                    image_filename='paypal.png', image_size=(80, 50), image_subsample=2, border_width=0)],
+               [sg.Output(size=(76, 10), font=('Helvetica', 11), key='out')],
+               [sg.Text('Enter seed phrase or to overview address:', font=('Helvetica', 8), size=(15,1))],
+               [sg.Multiline(size=(76,3), font=('Helvetica', 11), key = 'gen')],
+               [sg.Button('Generate wallet', font=('Helvetica', 11)),sg.Button('Overview address', font=('Helvetica', 11))]]
+
+    return sg.Window('myCoin',
+                     layout=layout,
+                     default_element_size=(30, 2),
+                     font='Helvetica 18',
+                     right_click_menu=right_click_menu)
+def main():
+    window, settings = None, load_settings(SETTINGS_FILE, DEFAULT_SETTINGS )
+    while True:
+        if window is None:
+            window = create_main_window(settings)
+        event, values = window.Read()
+        f = values['gen'].rstrip()
+        if event == 'Generate wallet':
+            salt = sal()
+            secret_exponent = seed(f, salt)
+            public_key = pub_key(secret_exponent)
+            address = addr(public_key)
+            WIF = wif(secret_exponent)
+            balance = bala(address)
+            overv = over(address)
+            data = open("myCoin-Wallet.docx","a")
+            data.write("myCoin-Wallet: "+"\n\n" +
+                       "Seed phrase: " +str(f)+' '+str(salt)+"\n\n"+
+                       "Privatekey: " +str(secret_exponent)+"\n\n"+
+                       "Publickey:  " + str(public_key)+"\n\n"+
+                       "WIF:        " +str(WIF)+"\n\n"+
+                       "Address:    "+str(address)+"\n\n"+
+                       "Balance:    "+str(balance)+' '+'btc'+"\n\n"+
+                       "Transactions:  " +str(overv)+"\n"+
+                       '-------------------------------------------------------------------------------------------------------------'+"\n\n")
+            data.close()
+            print("Seed phrase:  " +str(f)+ ' ' +str(salt)+"\n"+
+                  "Privatekey:      " +str(secret_exponent)+"\n"+
+                  "WIF:                " +str(WIF)+"\n"+
+                  "Address:         "+str(address)+"\n"+
+                  "Balance:          "+str(balance)+' '+'btc'+"\n"+
+                  "Transactions:  " +str(overv)+"\n")
+
+        elif event == 'Overview address':
+            d = blockcypher.get_address_overview(f)
+            print(d)
+                  
+        elif event == 'Copy':
+            pyperclip.copy(str(f))
+            pyperclip.paste()
+
+        elif event == 'Paste':
+            text = pyperclip.paste()
+            window.Element('gen').Update(str(text))
+
+        elif event == 'Settings':
+            event, values = create_settings_window(settings).read(close=True)
+            if event == 'Save':
+                window.close()
+                window = None
+                save_settings(SETTINGS_FILE, settings, values)
+
+        elif event == 'About...':
+            sg.popup('About:', 'Created by A. Petek', 'myCoin bitcoin brainwallet', 'Version 1.1',)
+
+        elif event == 'paypal':
+            webbrowser.open_new_tab("https://www.paypal.com/donate/?cmd=_s-xclick&hosted_button_id=PFB6A6HLAQHC2&source=url")
+        
+
+
+
+        elif event in (None, 'Exit'):
+            break
+
+    window.Close()    
+
+main()
+
---
+++ readme.md
@@ -0,0 +1,133 @@
+# my Coin Bitcoin brainwallet 
+
+[![forthebadge made-with-python](http://ForTheBadge.com/images/badges/made-with-python.svg)](https://www.python.org/)
+
+[![Open Source Love svg1](https://badges.frapsoft.com/os/v1/open-source.svg?v=103)](https://github.com/adrijano/myCoin-Brainwallet/)
+
+[![Maintenance](https://img.shields.io/badge/Maintained%3F-yes-green.svg)](https://github.com/adrijano/myCoin-Brainwallet/graphs/commit-activity)
+
+![myCoin](myCoin.gif)
+
+![Adrijan's github stats](https://github-readme-stats.vercel.app/api?username=adrijano&show_icons=true)
+
+[![paypal](https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif)](https://www.paypal.com/donate/?cmd=_s-xclick&hosted_button_id=PFB6A6HLAQHC2&source=url)
+
+# If you like it give it a star
+
+[![GitHub stars](https://img.shields.io/github/stars/adrijano/myCoin-Brainwallet.svg?style=social&label=Star&maxAge=2592000)](https://github.com/adrijano/myCoin-Brainwallet/)
+
+**Programmed in Python | PySimpleGUI**
+
+# How it works
+```
+From seed phrase and salt everytime create different bitcoin
+privatekey and then convert it to Wallet Interchange Format key (WiF) format, 
+which is a Base-58 form for the random key. 
+This is the format that is stored in the Bitcoin Wallet. 
+
+
+For example a sample private key is generated from:
+
+seed phrase and salt:   correct horse battery staple
+9f0854ceafcdaeda
+
+Privatekey:  
+
+632c14b35749421e6066121f8fd2f54372e23b20be4288fd1cf4794def0a5753
+
+
+We then convert this into WiF format (Base-58):
+
+5JZxoUFisVfYGXgbVnoHJA7jMWJHxaMg2WYpERLM9SeCtD9M1yt
+
+This can be stored in a Bitcoin wallet. Next we can take then private key and a 
+hash value, and covert it into a useable Bitcoin address, such as:
+
+17bEVEDSBCR2SzS6epa3BdMXqSfQd1Ec4j
+
+The format of the keys is defined below, where we create a 256-bit private key 
+and convert this to a WiF private key. Next we generate a 512-bit public key, 
+and then take a 160-bit RIPEM-160 hash and convert to a Bitcoin address.
+```
+### Address overview
+```
+Paste address and click Overview address you want.
+
+```
+# How to use
+
+### Python3+
+```
+git clone https://github.com/adrijano/myCoin-Brainwallet.git
+
+cd myCoin-Brainwallet && pip install -r requirements.txt
+
+python3 myCoin.py
+```
+## Sample
+
+**myCoin-Wallet is saved in myCoin-Wallet.docx**
+
+```
+-------------------------------------------------------------------------------------------------------------------------------
+
+myCoin-Wallet: 
+
+Seed phrase: correct horse battery staple 9f0854ceafcdaeda
+
+Privatekey: 632c14b35749421e6066121f8fd2f54372e23b20be4288fd1cf4794def0a5753
+
+Publickey:  04c742fc44fa02739088ff67fdc2363e8a3d14b1b60e13f30f3a266e5d7ff6d908f1d625c4cbb8ee532307552df1d2e2791723f9a62048c7ec643470d875032d53
+
+WIF:        5JZxoUFisVfYGXgbVnoHJA7jMWJHxaMg2WYpERLM9SeCtD9M1yt
+
+Address:    17bEVEDSBCR2SzS6epa3BdMXqSfQd1Ec4j
+
+Balance:    0 btc
+
+Transactions:  0
+-------------------------------------------------------------------------------------------------------------------------------
+
+Wallet: 
+
+Seed phrase: correct horse battery staple fe4aa5d13bfd4e3f
+
+Privatekey: 240dc0a82556215c2dcb1997d45d2b97ac1f7bc168234dc702e27b2448fa54a7
+
+Publickey:  041c4eefce239530de78b59361d6287e8ac183ab69398e8ffa6a3c0501a2de617a420c8ff2edb3bfeab46ca27a1c47ddda915f7e5f262c2a537399d566fd4741df
+
+WIF:        5J6AXRmnSU5P3cNSxaCzgaeby5pkKew2sCpF3Q8SsBHqnp7ftZX
+
+Address:    1DownQ4WFcnc9RtC4AKHcwa7Eyj15vFVbK
+
+Balance:    0 btc
+
+Transactions:  0
+-------------------------------------------------------------------------------------------------------------------------------
+
+```
+![myCoin](Capture1.PNG)
+![myCoin](Capture9.PNG)
+
+## Donations
+If you would like to support me, donations are very welcome.
+
+```
+You can use Paypal to donate using your own credit card. 
+The payment is processed by PayPal but you don't need to have a
+PayPal account or sign-up for one if you are paying by credit card.
+
+You can also use your own Paypal account to donate.
+```
+[![paypal](https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif)](https://www.paypal.com/donate/?cmd=_s-xclick&hosted_button_id=PFB6A6HLAQHC2&source=url)
+
+[![ForTheBadge built-with-love](http://ForTheBadge.com/images/badges/built-with-love.svg)](https://github.com/adrijano/myCoin-Brainwallet/)
+
+
+
+# Disclaimer
+
+
+**The code within this repository comes with no guarantee, the use of this code is your responsibility. I take NO responsibility and/or liability for how you choose to use any of the source code available here. By using any of the files available in this repository, you understand that you are AGREEING TO USE AT YOUR OWN RISK. Once again, ALL files available here are for EDUCATION and/or RESEARCH purposes ONLY.**
+
+
